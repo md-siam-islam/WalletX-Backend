46ea002293f25fb6049512850497bc7ed48b90f4
md-siam-islam <mdsiamislam663@gmail.com>
2025-10-07 21:19:48 +0600

    user zod data create done

---
+++ src/app/modules/User/user.validate.ts
@@ -0,0 +1,33 @@
+ import z from "zod";
+
+ import { UserRole, UserStatus } from "./user.interface";
+
+ export const CreateUserZodSchema = z.object({
+
+    name: z
+    .string({ message: "Name must be a string" })
+    .min(3, { message: "Name must be at least 3 characters" })
+    .max(50, { message: "Name must be at most 50 characters" }),
+
+    email: z
+    .string({ message: "Email must be a string" })
+    .email("Invalid email format"),
+    phone: z
+    .string({ message: "Phone number must be a string" })
+    .regex(/^(\+8801|8801|01)[3-9]\d{8}$/, {
+      message: "Phone number must be a valid Bangladeshi number",
+    }),
+    password: z
+    .string({ message: "Password must be a string" })
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(100, { message: "Password must be at most 100 characters" }),
+
+    address: z.string().optional(),
+    picture: z.string().optional(),
+    role: z.enum([UserRole.ADMIN, UserRole.AGENT, UserRole.USER]).default(UserRole.USER),
+    isActive: z.enum([UserStatus.ACTIVE, UserStatus.INACTIVE, UserStatus.PENDING, UserStatus.SUSPENDED]).default(UserStatus.ACTIVE),
+    isVerified: z.boolean().default(true),
+    isDeleted: z.boolean().default(false),
+    walletId: z.string().optional(),
+    walletBalance: z.number().min(0).optional()
+ })
