3bf3965f19da1871180cbd4f4c55505ab954ef78
md-siam-islam <mdsiamislam663@gmail.com>
2025-10-10 22:34:46 +0600

    User route modify done

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -10,10 +10,10 @@ import { UserRole } from './user.interface';
  const router = express.Router();
 
 router.post('/create', userValidate(CreateUserZodSchema), UserController.Createuserwithwallet);
-router.get('/allUsers', UserController.getAllusers);
+router.get('/allUsers',checkAuth(UserRole.ADMIN), UserController.getAllusers);
 router.patch('/:id',userValidate(UpdateUserZodSchema),checkAuth(...Object.values(UserRole)), UserController.updateUser);
 
-// router.get('/:id', UserController.getSingleuser);
+router.get('/:id', UserController.getSingleuser);
 
 
 export const UserRoute = router;
